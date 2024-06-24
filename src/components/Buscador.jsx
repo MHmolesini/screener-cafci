3a5dc0fff263227d699ca60254ab29e7d03a24a9
Matias Molesini <Mhmolesini@gmail.com>
2024-06-23 21:19:04 -0300

    Estilos al componente de Buscador.jsx

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-import '../styles/components/Buscador.css';
+import "../styles/components/Buscador.css";
+import Search from "../images/icons/Search.svg";
 
 const Buscador = () => {
   return (
     <>
-      <div className='Buscador'>Buscador</div>
+      <div className="Buscador">
+        <div className="Buscador_interior">
+          <div className="Icon">
+            <img src={Search} alt="Search" />
+          </div>
+          <input type="text" placeholder="Search" />
+        </div>
+      </div>
     </>
   );
 };
